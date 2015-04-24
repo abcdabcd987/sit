2e3791d34984e1797040e181ac2ad16c54ece27f
BreakVoid <breakvoid@hotmail.com>
2015-04-24 08:35:41 +0800

    use recursive_directory_iterator to solve the problem that add the whole folder

--- src/sit-add.cc
+++ src/sit-add.cc
@@ -9,13 +9,38 @@ void AddFile(const boost::filesystem::path &file)
 		exit(0);
 	}
 	try {
-		Util::safe_copy(file, repo_dir / "stage" / file);
-	} catch (const filesystem_error &ec) {
-		std::cerr << "Failed to copy " << file << " to stage" << std::endl;
-		std::cerr << ec.what() << std::endl;
+		if (!is_directory(file)) {
+			Util::safe_copy_file(file, stage_dir / file);
+		} else {
+			for (recursive_directory_iterator end, cur(file); cur != end; ++cur) {
+				if (is_directory(cur->path())) {
+					continue;
+				}
+				Util::safe_copy_file(cur->path(), stage_dir / cur->path());
+			}
+		}
+	} catch (const filesystem_error &fe) {
+		std::cerr << fe.what() << std::endl;
+	} catch (const std::exception &stdEc) {
+		std::cerr << stdEc.what() << std::endl;
 	}
 #ifdef WIN32
-	Util::for_all(repo_dir / "stage" / file, &Util::dos2unix);
+	try {
+		if (!is_directory(file)) {
+			Util::dos2unix(file);
+		} else {
+			for (recursive_directory_iterator end, cur(stage_dir / file); cur != end; ++cur) {
+				if (is_directory(cur->path())) {
+					continue;
+				}
+				Util::dos2unix(cur->path());
+			}
+		}
+	} catch (const filesystem_error &fe) {
+		std::cerr << fe.what() << std::endl;
+	} catch (const std::exception &stdEc) {
+		std::cerr << stdEc.what() << std::endl;
+	}
 #endif
 }
 }
